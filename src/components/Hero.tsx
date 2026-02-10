ddc9867aa75f4a89e862676e2f9e10e123df8f34
Zariel <zariel.nd@gmail.com>
2026-02-10 14:38:29 +0700

    Enhance video frame performance by applying GPU acceleration and optimizing transform styles

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -81,7 +81,7 @@ const Hero = () => {
     >
       <div
         id="video-frame"
-        className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-black will-change-transform"
+        className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-black will-change-transform transform-gpu"
       >
         <video
           src={videoSrc}
@@ -90,7 +90,7 @@ const Hero = () => {
           muted
           playsInline
           className="absolute left-0 top-0 size-full object-cover object-center"
-          style={{ willChange: "transform" }}
+          style={{ willChange: "transform", transform: "translateZ(0)" }}
         />
         <div
           id="content"
