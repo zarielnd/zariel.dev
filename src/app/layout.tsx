f157eba9b75b2b27e5e33f3573a4322406519ccc
Zariel <zariel.nd@gmail.com>
2026-02-10 11:51:47 +0700

    Remove unused SplashScreen import from layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import SplashScreen from "@/components/SplashScreen";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
